3f434e4fc4a5eb68428cbc62d891ddf1c441158a
LAPTOP-PRAN7OVE\zhangbin <zhangqiyi1@163.com>
2023-04-18 20:09:14 +0800

    1

---
+++ src/main/java/com/beaver/live/LiveApplication.java
@@ -0,0 +1,17 @@
+package com.beaver.live;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: springboot_live
+ * @description:
+ * @author: zhb
+ * @create: 2023-04-15 09:45
+ */
+@SpringBootApplication
+public class LiveApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LiveApplication.class, args);
+    }
+}
